c57cb2608dc0eac024923f26e504067c0ac990c2
Iggy <ilgyo87@gmail.com>
2025-06-07 01:15:41 -0400

    working version!!

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Button, View, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import { Amplify } from "aws-amplify";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import outputs from "./amplify_outputs.json";
 
 Amplify.configure(outputs);
@@ -18,17 +20,27 @@ const SignOutButton = () => {
 
 const App = () => {
   return (
-    <Authenticator.Provider>
-      <Authenticator>
-        <SignOutButton />
-      </Authenticator>
-    </Authenticator.Provider>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="auto" />
+        <Authenticator.Provider>
+          <Authenticator>
+            <SignOutButton />
+          </Authenticator>
+        </Authenticator.Provider>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
   signOutButton: {
     alignSelf: "flex-end",
+    margin: 20,
   },
 });
 
